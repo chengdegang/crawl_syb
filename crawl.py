a1c64341c90924596d9a6614ac872d5208747870
ezxr.chengdegang <chengdegang@ezxr>
2021-05-23 15:59:29 +0800

    suc_v1

--- crawl.py
+++ crawl.py
@@ -8,19 +8,19 @@ import bs4
 import re
 
 # webbrowser.open('http://www.shiyebian.net/zhejiang/hangzhou/')
-res = requests.get('http://www.shiyebian.net/zhejiang/hangzhou/')
+# res = requests.get('http://www.shiyebian.net/zhejiang/hangzhou/')
 # print(type(res))
 # res.status_code == requests.codes.ok
-res.raise_for_status()
+# res.raise_for_status()
 #将页面源码写入本地
-with open('data/result.txt','wb') as result:
-    for line in res.iter_content(200000):
-        result.write(line)
-result.close()
+# with open('data/result.txt','wb') as result:
+#     for line in res.iter_content(200000):
+#         result.write(line)
+# result.close()
 
 #转成bs4，方便提取html信息
-soup = bs4.BeautifulSoup(open('data/result.txt','r',encoding='gbk'), 'html.parser')
-print(type(soup))
+# soup = bs4.BeautifulSoup(open('data/result.txt','r',encoding='gbk'), 'html.parser')
+# print(type(soup))
 # elements = soup.select('div')
 # print(len(elements))
 #查找每日公告
@@ -30,17 +30,33 @@ print(type(soup))
 # info_text = info[0].getText()
 # print(info_text)
 
-#获取前五十个招考信息
+#获取前五十个招考信息,作为一个新的列表
 def get_50():
+    res = requests.get('http://www.shiyebian.net/zhejiang/hangzhou/')
+    res.raise_for_status()
+    # 将页面源码写入本地
+    with open('data/result.txt', 'wb') as result:
+        for line in res.iter_content(200000):
+            result.write(line)
+    result.close()
+    soup = bs4.BeautifulSoup(open('data/result.txt', 'r', encoding='gbk'), 'html.parser')
     result_2 = []
     i = 1
     while i <= 50:
         info = soup.select(
             f'body > div.main > div.listl.fl > div.listrr.fr > div > div.listlie > ul> li:nth-child({i})')
-        print(info)
+        info = str(info)
         i = i + 1
+        #不去除【】，现去除后格式不一致影响判断
+        # info = info.lstrip('[')
+        # info = info.rstrip(']')
+        # print(info)
         result_2.append(info)
+    # 去掉列表括号
+    # result_2 = ''.join(result_2)
+    # print(result_2)
     return result_2
+
 
 # print(result_2)
 # body > div.main > div.listl.fl > div.listrr.fr > div > div.listlie > ul> li:nth-child(6)
@@ -60,10 +76,10 @@ def reopen():
             #去掉换行符
             line = line.strip('\n')
             reopen_list.append(line)
-    print(reopen_list)
+    # print(reopen_list)
     return reopen_list
 
-#传入两个列表，输出需要的列表
+#传入两个列表，输出需要的列表1
 def judge(listnew,listold):
     a = [x for x in listnew if x in listold]  # 两个列表表都存在
     b = [y for y in (listnew + listold) if y not in a]
@@ -71,38 +87,65 @@ def judge(listnew,listold):
     # print(f'notsame is:{b}')
     c = []
     for z in b:
-        if z in listnew:
-            c.append(z)
+        if z in listnew and z in listold :
+            continue
+        else:
+            if z in listnew:
+                c.append(z)
     print(c)
     return c
 
-#将要输出的数据进行处理
-def organize_data():
-    listces = [
+#传入两个列表，输出需要的列表2,简单方式
+def judge2(listnew,listold):
+    c = []
+    for z in listnew:
+        if z in listold:
+            continue
+        else:
+            c.append(z)
+
+    # print(c)
+    return c
+
+#list参考格式
+listces = [
         '[<li><em>05-14</em><a href="http://www.shiyebian.net/xinxi/376489.html" target="_blank">2021年杭州住房公积金管理中心桐庐分中心招聘编外人员公告</a></li>]',
         '[<li><em>05-14</em><a href="http://www.shiyebian.net/xinxi/376488.html" target="_blank">2021年杭州市桐庐县商务局（桐庐迎春商务区管理委员会）招聘编外人员公告</a></li>]',
         '[<li><em>05-13</em><a href="http://www.shiyebian.net/xinxi/376344.html" target="_blank">2021年杭州市桐庐县行政服务中心招聘编外人员公告</a></li>]']
-    for line in listces:
+#将要输出的数据进行处理
+def organize_data(list):
+    res = []
+    for line in list:
+        #通过正则匹配每行对应的内容
         time = re.findall(r"<li><em>(.+?)</em>", line)
+        time = ''.join(time)
         url = re.findall(r'href="(.+?)" target', line)
+        url = ''.join(url)
         title = re.findall(r'_blank">(.+?)</a', line)
-        print(f'时间：{time}+url：{url}+主题：{title}')
+        title = ''.join(title)
+        mes =  f'更新日期:{time}  url:{url}  主题:{title}'
+        # print(mes)
+        res.append(mes)
+    # print(res)
+    return res
 
-def sendmail():
+def sendmail(mesg):
     smtpserver = 'smtp.qq.com'
     username = 'chengdgccc@qq.com'
     password = 'glgfdpxlnhiogaji'
     sender = 'chengdgccc@qq.com'
-    receivers = ['chengdegang@ezxr.com']
+    receivers = ['18868890069@163.com']
 
     subject = 'syb reminder'
 
     msg = MIMEMultipart('mixed')
     msg['Subject'] = subject
     msg['From'] = 'QA_CCC'
-    msg['To'] = 'qa'
+    msg['To'] = 'ccc'
 
-    text = "Hi!\nHow are you?\nHere is the link you wanted:\nhttp://www.baidu.com\nces1018"
+    #处理入参数据
+
+    text = f"Hi!\nThis is the mes u care about:\n{mesg}"
     text_plain = MIMEText(text, 'plain', 'utf-8')
     msg.attach(text_plain)
 
@@ -113,6 +156,22 @@ def sendmail():
     print("send success")
     smtp.quit()
 
+#第一步，获取当日最新的数据
+# get_50()
+#第二步，将当日的数据与昨日的数据对比并处理结果数据到可用程度
+result = judge2(get_50(),reopen())
+#第四步，做判断，若result有更新即大于0，则运行组织函数及发信函数
+if len(result) > 0 :
+    print("今天更新了")
+    result2 = organize_data(result)
+    sendmail(result2)
+else:
+    print("今天没有更新")
+
+#第五步，保存今日的数据并覆盖历史数据
 # record(get_50())
-# reopen()
-organize_data()
+
+# organize_data(listces)
+
+# result2 = organize_data(listces)
+# sendmail(result2)
